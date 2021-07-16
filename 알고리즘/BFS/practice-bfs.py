d4fa6c5889dc709991c841e7f2d09dafe56af3c4
junseo <ysj6844@naver.com>
2021-07-16 16:09:56 +0900

    BFS

---
+++ 알고리즘/BFS/practice-bfs.py
@@ -0,0 +1,27 @@
+def bfs(graph, startNode):
+    visited = list()
+    needVisit = list()
+
+    needVisit.append(startNode)
+
+    while needVisit:
+        node = needVisit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            needVisit.extend(graph[node])
+    return visited
+
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(bfs(graph, 'A'))
