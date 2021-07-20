ffa07321d88f7783524c9ebd549a520b064627e0
junseo <ysj6844@naver.com>
2021-07-20 16:42:18 +0900

    dijkstra

---
+++ 알고리즘/최단 경로/dijkstra-practice.py
@@ -0,0 +1,32 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+
+    while queue:
+        currentDistance, currentNode = heapq.heappop(queue)
+
+        if distances[currentNode] < currentDistance:
+            continue
+
+        for adjacent, weight in graph[currentNode].items():
+            distance = currentDistance + weight
+
+            if distance < distances[adjacent]:
+                distances[adjacent] = distance
+                heapq.heappush(queue, [distance, adjacent])
+    return distances
+
+
+myGraph = {
+    'A': {'B': 8, 'C': 1, 'D': 2},
+    'B': {},
+    'C': {'B': 5, 'D': 2},
+    'D': {'E': 3, 'F': 5},
+    'E': {'F': 1},
+    'F': {'A': 5}
+}
+print(dijkstra(myGraph, 'A'))
